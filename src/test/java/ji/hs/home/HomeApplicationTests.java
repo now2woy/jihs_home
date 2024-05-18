36fd0f5357986888235802a6a1cca257a7f1f440
now2w <now2w@CHS-MINIPC>
2024-05-18 17:39:48 +0900

    .

---
+++ src/test/java/ji/hs/home/HomeApplicationTests.java
@@ -0,0 +1,13 @@
+package ji.hs.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
